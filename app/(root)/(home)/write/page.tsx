01b6976af57b3e3690487fe8fcd8a6214963f837
ShinobUu22 <saphin1234@gmail.com>
2024-07-10 19:57:32 +0545

    updated

--- app/(root)/(home)/write/page.tsx
+++ app/(root)/(home)/write/page.tsx
@@ -1,3 +1,5 @@
+// page.tsx
+
 "use client";
 
 import { Button } from "@/components/ui/button";
@@ -5,32 +7,35 @@ import React, { useState } from "react";
 import { CiImageOn } from "react-icons/ci";
 import { FaUpload, FaVideo } from "react-icons/fa";
 import { IoAdd } from "react-icons/io5";
-import ReactQuill from "react-quill";
+import dynamic from 'next/dynamic';
 import "react-quill/dist/quill.bubble.css";
+
+// Dynamically import ReactQuill with no SSR
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+
 const WritePage = () => {
   const [open, setOpen] = useState<Boolean>(false);
   const [value, setValue] = useState("");
+  
   return (
-    <div className="flex flex-col items-center ">
+    <div className="flex flex-col items-center">
       <input
         type="text"
         placeholder="Title"
-        className=" w-4/5 rounded-xl text-center p-6 font-semibold text-[64px]"
+        className="w-4/5 rounded-xl text-center p-6 font-semibold text-[64px]"
       />
       <div className="h-fit flex flex-col gap-8 mt-8 w-full">
         <div className="flex gap-8">
           <IoAdd
             size={25}
             onClick={() => setOpen(!open)}
-            className=" cursor-pointer"
+            className="cursor-pointer"
           />
 
           {open && (
             <div className="flex gap-8">
               <CiImageOn size={25} className="cursor-pointer"/>
-
               <FaUpload size={25} className="cursor-pointer"/>
-
               <FaVideo size={25} className="cursor-pointer"/>
             </div>
           )}
@@ -40,11 +45,10 @@ const WritePage = () => {
           theme="bubble"
           value={value}
           onChange={setValue}
-          
           className="quill text-xl"
         />
       </div>
-      <Button className="bg-green-400 my-10 ">Publish</Button>
+      <Button className="bg-green-400 my-10">Publish</Button>
     </div>
   );
 };
